e0f2c1c96b2b23505b2111d225f1df8cb7181a49
Vinicius Matos <163915966+viniciusfelixmatos@users.noreply.github.com>
2025-08-20 21:19:34 -0300

    feat: Correção no javascript e adições ao layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,27 @@
 const url = "https://dadosabertos.camara.leg.br/api/v2/deputados?siglaUf=CE&ordem=ASC&ordenarPor=nome";
 
-
-const deputadosarea = document.getElementById("deputadocard");
-const deputadobox = document.querySelector("#deputados-card");
+const templatecard = document.getElementById("deputadocard");
+const deputadoslist = document.getElementById("deputados-list");
 
 async function getAllInfo() {
-    const response = await fetch (url);
+    const response = await fetch(url);
     console.log(response);
 
     const data = await response.json();
     console.log(data);
 
-    deputadosarea.classList.add("hide");
+    data.dados.map((deputado) => {
+        const card = templatecard.cloneNode(true);
+        card.style.display = "flex";
+        card.removeAttribute("id");
+        card.classList.remove("template");
 
-    data.map((post) => {
-        const name = document.createElement("p");
-        const partido = document.createElement("p");
+        card.querySelector(".cardimg").src = deputado.urlFoto;
+        card.querySelector(".card-name").innerText = deputado.nome;
+        card.querySelector(".card-party").innerText = `(${deputado.siglaPartido})`;
 
-        name.innterText = post.nome;
-        partido.innerText = post.siglaPartido;
-
-        deputadobox.appendChild(name);
-        deputadobox.appendChild(partido);
-    })
+        deputadoslist.appendChild(card);
+    });
 }
 
 getAllInfo();
