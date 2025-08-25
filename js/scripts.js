8e924e53b0faf2d0a48ef5227b643dafb2a1668a
Vinicius Matos <163915966+viniciusfelixmatos@users.noreply.github.com>
2025-08-25 18:46:31 -0300

    feat: Correções no layout e complemento no js

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,42 @@ const templatecard = document.getElementById("deputadocard");
 const deputadoslist = document.getElementById("deputados-list");
 
 async function getAllInfo() {
-    const response = await fetch(url);
-    console.log(response);
+  const response = await fetch(url);
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
 
-    const data = await response.json();
-    console.log(data);
+  data.dados.forEach((deputado) => {
+    const card = templatecard.cloneNode(true);
+    card.style.display = "flex";
+    card.removeAttribute("id");
+    card.classList.remove("template");
 
-    data.dados.map((deputado) => {
-        const card = templatecard.cloneNode(true);
-        card.style.display = "flex";
-        card.removeAttribute("id");
-        card.classList.remove("template");
+    card.querySelector(".cardimg").src = deputado.urlFoto;
+    card.querySelector(".card-name").innerText = deputado.nome;
+    card.querySelector(".card-party").innerText = `(${deputado.siglaPartido})`;
 
-        card.querySelector(".cardimg").src = deputado.urlFoto;
-        card.querySelector(".card-name").innerText = deputado.nome;
-        card.querySelector(".card-party").innerText = `(${deputado.siglaPartido})`;
-
-        deputadoslist.appendChild(card);
+    // 🔹 Listener direto no card
+    card.addEventListener("click", () => {
+      loadDeputado(deputado);
     });
+
+    deputadoslist.appendChild(card);
+  });
 }
 
 getAllInfo();
 
+function loadDeputado(deputado) {
+  document.getElementById("modal-foto").src = deputado.urlFoto;
+  document.getElementById("modal-nome").textContent = deputado.nome;
+  document.getElementById("modal-partido").textContent = deputado.siglaPartido;
+  document.getElementById("modal-uf").textContent = deputado.siglaUf;
+  document.getElementById("modal-sexo").textContent = "Não disponível";
+  document.getElementById("modal-nascimento").textContent = "Não disponível";
+  document.getElementById("modal-cpf").textContent = "Não disponível";
+  document.getElementById("modal-email").textContent = deputado.email ?? "Não disponível";
+  document.getElementById("modal-escolaridade").textContent = "Não disponível";
+  document.getElementById("modal-nomecivil").textContent = "Não disponível";
 
+}
